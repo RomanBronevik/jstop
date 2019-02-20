075755559924091b9ba18ccfef2f6cc31cbd1853
Roman <roman.bronevik@ya.ru>
2019-02-20 19:36:09 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>jstop.heroku.com</h1>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        mounted() {
+            console.log('index page')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
